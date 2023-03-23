f28f716bae566dfebdcd71412abb87c3cf9c9813
Pasha Khromchenko <pavelkhrcs@gmail.com>
2023-03-23 03:04:08 -0400

    Added crude queue functionality

--- music.py
+++ music.py
@@ -1,9 +1,9 @@
 import discord
 from discord.ext import commands
-import asyncio
 import os
 import yt_dlp
 import logging
+import asyncio
 
 logger = logging.getLogger('my_bot')
 
@@ -14,7 +14,7 @@ intents = discord.Intents.all()
 # Create a new Bot instance with command prefix "." and intents
 bot = commands.Bot(command_prefix='.', intents=intents)
 
-# vc join Function
+# VC join Function
 async def join_voice_channel(ctx):
     # Check if the user is in a voice channel
     if not ctx.message.author.voice:
@@ -39,7 +39,7 @@ async def play_song(ctx, url):
     # Check if the "songs" directory exists, if not create it
     if not os.path.exists("songs"):
         os.makedirs("songs")
-    # Get the bot's voice client for the server the command was invoked on
+    # Get the bot's voice client for where the command was invoked
     voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
     # Check if a song is already playing
     song_there = os.path.isfile("songs/song.mp3")
@@ -62,7 +62,7 @@ async def play_song(ctx, url):
         await ctx.send("Wait for the current playing music to end or use the 'stop' command.")
         return
     # Download the song from YouTube
-    await ctx.send("Downloading song...")
+    await ctx.send("Downloading song files...")
     ydl_opts = {
         'format': 'bestaudio/best',
         'outtmpl': 'songs/%(title)s.%(ext)s',
@@ -73,6 +73,7 @@ async def play_song(ctx, url):
         }]
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        # Uses ydl to extract the title
         info_dict = ydl.extract_info(url, download=True)
         title = info_dict.get('title', 'song')
     # Play the song
@@ -80,29 +81,62 @@ async def play_song(ctx, url):
     if voice is None:
         await ctx.send("I am not currently connected to a voice channel.")
         return
+    # Creates an anonymous function, prints error message
     voice.play(discord.FFmpegPCMAudio(f"songs/{title}.mp3"), after=lambda e: print(f"Error: {e}") if e else None)
     # Set the volume of the song to 7%
     voice.source = discord.PCMVolumeTransformer(voice.source)
     voice.source.volume = 0.07
     # Send a message to the user indicating that the song is now playing
-    await ctx.send(f"Now playing: {title}")
+    await ctx.send(f"*Now playing: {title} 🎵*")
+    
+# Initialize queue array
+queue = []
 
+# Play next song function
+async def play_next(ctx):
+    # Check if queue is empty
+    if queue:
+        # Get first song
+        next_song = queue[0]
+        # Remove first song before playing it
+        queue.pop(0)
+        # Play the next song
+        await play_song(ctx, next_song)
+    # If queue is empty, disconnect
+    else:
+        voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+        await voice.disconnect()
+        
 # Play command
 @bot.command()
 async def play(ctx, url):
-    # Invoke the play_song function with the provided URL
-    await play_song(ctx, url)
-
+    # Add URL to queue
+    queue.append(url)
+    # If queue length is 1, play it
+    if len(queue) == 1:
+        await play_song(ctx, queue[0])
+    else:
+        # Indicate that song has been added
+        await ctx.send(f"{url} has been added to the queue.")
+        
+# Skip command
+@bot.command()
+async def skip(ctx):
+    # Get the voice client info
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    # Stop playing audio
+    voice.stop()
+    # Play the next song in queue
+    await play_next(ctx)
+        
 # Stop command
 @bot.command()
 async def stop(ctx):
     # Get the voice client for the server the command was sent from
     voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-    # Check if the voice client is currently playing audio
-    if voice.is_playing():
-        # Stop playing audio
-        voice.stop()
+    # Leave voice
+    voice.stop()
     # Disconnect from the voice channel
     await voice.disconnect()
 
-bot.run('TOKEN')
+bot.run('MTA4Nzg0NzM3ODMxODUzMjY5OQ.GQ07Hi.2qKaPA9T7s8tqH-828dHjUeI5PFX4_qYCewz8w')
