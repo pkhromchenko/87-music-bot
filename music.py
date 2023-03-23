fb370af258135f53fc3f002e3774e47653a07d00
Pasha Khromchenko <pavelkhrcs@gmail.com>
2023-03-23 05:45:48 -0400

    Added queue functionality

--- music.py
+++ music.py
@@ -7,7 +7,6 @@ import asyncio
 
 logger = logging.getLogger('my_bot')
 
-
 # Create a Discord Intents object that enables all events to be received
 intents = discord.Intents.all()
 
@@ -20,10 +19,10 @@ async def join_voice_channel(ctx):
     if not ctx.message.author.voice:
         await ctx.send("You're not in a voice channel.")
         return False
-    
+
     # Get the voice channel object
     channel = ctx.message.author.voice.channel
-    
+
     # Connect to the voice channel
     try:
         voice_client = await channel.connect()
@@ -31,7 +30,7 @@ async def join_voice_channel(ctx):
     except Exception as e:
         await ctx.send(f"Failed to connect to voice channel: {e}")
         return False
-    
+
     return True
 
 # Play Function
@@ -39,10 +38,13 @@ async def play_song(ctx, url):
     # Check if the "songs" directory exists, if not create it
     if not os.path.exists("songs"):
         os.makedirs("songs")
+
     # Get the bot's voice client for where the command was invoked
     voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+
     # Check if a song is already playing
     song_there = os.path.isfile("songs/song.mp3")
+
     try:
         # If the bot is already connected to a voice channel, move to the user's channel
         if voice and voice.is_connected():
@@ -50,19 +52,22 @@ async def play_song(ctx, url):
         # If the bot is not connected to a voice channel, join the user's channel
         else:
             await join_voice_channel(ctx)
+
+            # Get the bot's voice client again after joining the channel
+            voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+
+        # If voice is None, the bot failed to join a voice channel
+        if voice is None:
+            await ctx.send("Failed to join a voice channel.")
+            return
+
     except:
         # If there was an error joining the voice channel, send a message to the user
         await ctx.send("Failed to join the voice channel.")
-    try:
-        # If there is a song playing, remove it
-        if song_there:
-            os.remove("songs/song.mp3")
-    except PermissionError:
-        # If there was an error removing the song file, send a message to the user
-        await ctx.send("Wait for the current playing music to end or use the 'stop' command.")
         return
+
     # Download the song from YouTube
-    await ctx.send("Downloading song files...")
+    await ctx.send("*Downloading song files...*")
     ydl_opts = {
         'format': 'bestaudio/best',
         'outtmpl': 'songs/%(title)s.%(ext)s',
@@ -74,21 +79,25 @@ async def play_song(ctx, url):
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         # Uses ydl to extract the title
-        info_dict = ydl.extract_info(url, download=True)
+        info_dict = ydl.extract_info(url, download=False)
         title = info_dict.get('title', 'song')
-    # Play the song
-    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-    if voice is None:
-        await ctx.send("I am not currently connected to a voice channel.")
-        return
-    # Creates an anonymous function, prints error message
-    voice.play(discord.FFmpegPCMAudio(f"songs/{title}.mp3"), after=lambda e: print(f"Error: {e}") if e else None)
-    # Set the volume of the song to 7%
-    voice.source = discord.PCMVolumeTransformer(voice.source)
-    voice.source.volume = 0.07
-    # Send a message to the user indicating that the song is now playing
-    await ctx.send(f"*Now playing: {title} 🎵*")
-    
+
+        # Check if the file already exists before downloading it
+        if not os.path.isfile(f"songs/{title}.mp3"):
+            ydl.download([url])
+
+    # Add the downloaded song to the queue
+    queue.append(title)
+
+    # If there is no song playing, start playing the queued songs
+    if not voice.is_playing():
+        await play_next(ctx)
+
+    # Send a message to the user indicating that the song has been added to the queue
+    await ctx.send(f"*Enqueued `{title}` in position `{len(queue)}`*")
+
+
+
 # Initialize queue array
 queue = []
 
@@ -100,25 +109,37 @@ async def play_next(ctx):
         next_song = queue[0]
         # Remove first song before playing it
         queue.pop(0)
-        # Play the next song
-        await play_song(ctx, next_song)
-    # If queue is empty, disconnect
-    else:
+
+        # Get the bot's voice client for where the command was invoked
         voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
-        await voice.disconnect()
-        
+
+        # Play the next song
+    try:
+        # Play the next song
+        voice.play(discord.FFmpegPCMAudio(f"songs/{next_song}.mp3"), after=lambda e: print(f"Error: {e}") if e else None)
+        # Set the volume of the song to 7%
+        voice.source = discord.PCMVolumeTransformer(voice.source)
+        voice.source.volume = 0.07
+        # Send a message to the user indicating that the next song is playing
+        await ctx.send(f"*Now playing: `{next_song}` 🎵*")
+    except:
+        # If there was an error playing the next song, send a message to the user
+        await ctx.send("Failed to play next song.")
+    else:
+        # If queue is empty
+        # Wait 60s before disconnecting
+        await asyncio.sleep(60)
+        # Check if queue is still empty, if so, disconnect
+        if not queue:
+            voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+            await voice.disconnect()
+
 # Play command
-@bot.command()
+@bot.command(aliases=['p'])
 async def play(ctx, url):
     # Add URL to queue
-    queue.append(url)
-    # If queue length is 1, play it
-    if len(queue) == 1:
-        await play_song(ctx, queue[0])
-    else:
-        # Indicate that song has been added
-        await ctx.send(f"{url} has been added to the queue.")
-        
+    await play_song(ctx, url)
+
 # Skip command
 @bot.command()
 async def skip(ctx):
@@ -128,7 +149,7 @@ async def skip(ctx):
     voice.stop()
     # Play the next song in queue
     await play_next(ctx)
-        
+
 # Stop command
 @bot.command()
 async def stop(ctx):
@@ -138,5 +159,5 @@ async def stop(ctx):
     voice.stop()
     # Disconnect from the voice channel
     await voice.disconnect()
-
+    
 bot.run('TOKEN')
